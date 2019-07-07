f07cb108c381d44c2ae53a6f2610322845ce423f
acha <323163497@qq.com>
2019-07-07 20:42:40 +0800

    增加form后，对保存按钮和textarea进行微调

--- static/js/changemode.js
+++ static/js/changemode.js
@@ -4,7 +4,7 @@ function changemode() {
     if ($('#editor').css('display') == 'none') {
         $('#frame').css('display','none');
         $('#editor').css('display','block');
-        $('#save').css('display','block');
+        $('#savebutton').css('display','block');
         $('#editor').load(frame.src.replace(/html/,'md'));
         $('#mdf').html('不保存');
         flag = 1;
@@ -12,7 +12,7 @@ function changemode() {
     else {
         $('#frame').css('display','block');
         $('#editor').css('display','none');
-        $('#save').css('display','none');
+        document.getElementById("savebutton").style.display="none";
         $('#mdf').html('修改');
         flag = 0;
     }
