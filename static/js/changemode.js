649c16d4b894e7d39c2d22bd7697345df323b089
acha <323163497@qq.com>
2019-07-07 15:46:40 +0800

    增加文章保存按钮，但暂无实际保存功能

--- static/js/changemode.js
+++ static/js/changemode.js
@@ -1,17 +1,19 @@
 var flag = 0;
 
-function changemode(button) {
+function changemode() {
     if ($('#editor').css('display') == 'none') {
         $('#frame').css('display','none');
         $('#editor').css('display','block');
+        $('#save').css('display','block');
         $('#editor').load(frame.src.replace(/html/,'md'));
-        button.innerHTML = '不保存';
+        $('#mdf').html('不保存');
         flag = 1;
     }
     else {
         $('#frame').css('display','block');
         $('#editor').css('display','none');
-        button.innerHTML = '修改';
+        $('#save').css('display','none');
+        $('#mdf').html('修改');
         flag = 0;
     }
 }
---
+++ static/js/save.js
@@ -0,0 +1,7 @@
+function save() {
+    $('#frame').css('display','block');
+    $('#editor').css('display','none');
+    $('#save').css('display','none');
+    $('#mdf').html('修改');
+    flag = 0;
+}
