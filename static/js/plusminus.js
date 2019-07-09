7cd09648b3e9bdc6557a53fd180c049f4f6327cf
acha <323163497@qq.com>
2019-07-09 19:07:09 +0800

    增加last变量使点击menubar时高亮只显示最近的一次点击

--- static/js/plusminus.js
+++ static/js/plusminus.js
@@ -1,3 +1,5 @@
+var last;
+
 function plusminus(button, tar) {
     loaded = 0;
     if (flag == 1) {
@@ -8,13 +10,14 @@ function plusminus(button, tar) {
     if (target.style.display != "none") {
         target.style.display = "none";
         button.firstChild.innerHTML = '+';
-        button.style.background = "rgb(245, 245, 245)";
     }
     else {
         target.style.display = "block";
         button.firstChild.innerHTML = '-';
-        button.style.background = "rgb(116, 236, 216)";
     }
+    if (last != undefined) last.style.background = "rgb(245, 245, 245)";
+    button.style.background = "rgb(116, 236, 216)";
+    last = button;
 
     $('#frame').attr('src','/article/' + tar);
 }
